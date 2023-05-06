8e8ab5a5cd9f526fe85696d06f17a388aecd2d0f
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-05-06 18:31:34 +0300

    fix square divs not filling the entire screen when <16

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// loop for making squared divs:
+
 let container = document.getElementsByClassName("largeContainer")[0];
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 100; i++) {
     let divContainer = document.createElement("div");
     divContainer.classList.add("divContainer");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 100; i++) {
         let divBlock = document.createElement("div");
         divBlock.classList.add("divBlock");
         divContainer.appendChild(divBlock);
@@ -11,8 +13,20 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(divContainer);
 }
 
+// painting them with black class:
+
 let divBlock = document.getElementsByClassName("divBlock");
 
 Array.from(divBlock).forEach(function(element) {
     element.addEventListener("mouseover", e => element.classList.add("divBlockColored"));
   });
+
+// slider functionality:
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
