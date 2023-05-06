d976d17a3794a1bf7fc3fc5f8d42a8570a9cce1b
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-05-06 18:56:31 +0300

    add buttons functionality, revise code for readibility, remove borders

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
-// loop for making squared divs:
-
-let container = document.getElementsByClassName("largeContainer")[0];
-
-for (let i = 0; i < 100; i++) {
-    let divContainer = document.createElement("div");
-    divContainer.classList.add("divContainer");
-    for (let i = 0; i < 100; i++) {
-        let divBlock = document.createElement("div");
-        divBlock.classList.add("divBlock");
-        divContainer.appendChild(divBlock);
-    }
-    container.appendChild(divContainer);
-}
-
-// painting them with black class:
-
-let divBlock = document.getElementsByClassName("divBlock");
-
-Array.from(divBlock).forEach(function(element) {
-    element.addEventListener("mouseover", e => element.classList.add("divBlockColored"));
-  });
-
 // slider functionality:
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
+  removeDivs();
+  countDivs();
+  paintBlack();
+}
+
+// loop for making squared divs and initial board:
+
+let container = document.getElementsByClassName("largeContainer")[0];
+
+countDivs();
+paintBlack();
+
+function countDivs(){
+  for (let i = 0; i < slider.value; i++) {
+      let divContainer = document.createElement("div");
+      divContainer.classList.add("divContainer");
+      for (let i = 0; i < slider.value; i++) {
+          let divBlock = document.createElement("div");
+          divBlock.classList.add("divBlock");
+          divContainer.appendChild(divBlock);
+      }
+      container.appendChild(divContainer);
+  }
+}
+// painting them with black class:
+
+function paintBlack(){
+let divBlock = document.getElementsByClassName("divBlock");
+Array.from(divBlock).forEach(function(element) {
+    element.addEventListener("mouseover", e => element.classList.add("divBlockColored"));
+  });
+}
+
+// function for removing:
+
+function removeDivs() {
+let elementsRemove = document.getElementsByClassName("divContainer");
+  Array.from(elementsRemove).forEach(function(element) {
+    element.remove();
+  })
+}
+
+// button reset
+
+let resetScreen = document.getElementsByClassName("reset")[0];
+resetScreen.onclick = function(){
+  removeDivs();
+  countDivs();
+  paintBlack();
 }
